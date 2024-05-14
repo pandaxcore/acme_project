47b4ad9f63ddd86950c7fb4401155c0dfd86c4f8
pandaxcore <pandadesign7@gmail.com>
2024-05-14 09:57:52 +0600

    initial III

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,11 +1,17 @@
+"""Docstring."""
 from django import forms
 
+from .models import Birthday
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label="Имя", max_length=20)
-    last_name = forms.CharField(
-        label="Фамилия", help_text="Необязательное поле", required=False
-    )
-    birtday = forms.DateField(
-        label="Дата рождения", widget=forms.DateInput(attrs={"type": "date"})
-    )
+
+class BirthdayForm(forms.ModelForm):
+    """Docstring."""
+
+    class Meta:
+        """Docstring."""
+
+        model = Birthday
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,12 @@
+"""Docstring."""
 from django.db import models
+
+
+class Birthday(models.Model):
+    """Docstring."""
+
+    first_name = models.CharField("Имя", max_length=20)
+    last_name = models.CharField(
+        "Фамилия", max_length=20, blank=True, help_text="Необязательное поле"
+    )
+    birthday = models.DateField('Дата рождения')
